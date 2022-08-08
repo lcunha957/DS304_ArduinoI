8d40799355276e0923fd596401882e9191434e3a
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-08-08 19:58:17 -0300

    pasta 11-05
    pasta 11-05, arduíno I

---
+++ Aula-11-05-22/sketch_may11b/sketch_may11b.ino
@@ -0,0 +1,37 @@
+//Carrega a biblioteca LiquidCrystal
+#include <LiquidCrystal.h>
+ 
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+void setup()
+{
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
+}
+ 
+void loop()
+{
+  //Limpa a tela
+  
+  //Posiciona o cursor na coluna 3, linha 0;
+  //Envia o texto entre aspas para o LCD
+ 
+   
+  //Rolagem para a esquerda
+  /*for (int posicao = 0; posicao < 3; posicao++)
+  {
+    
+    delay(300);
+  }*/
+   
+  //Rolagem para a direita
+  for (int posicao = 0; posicao < 6; posicao++)
+  {
+    
+    lcd.print("Teste direita");
+    delay(350);
+    lcd.clear();
+     delay(400);
+  }
+}
