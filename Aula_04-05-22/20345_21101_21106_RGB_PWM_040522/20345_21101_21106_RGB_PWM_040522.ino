d0255c6174944bf919145fbbe585bd65f2580f0c
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-08-08 19:57:38 -0300

    pasta da aula 04-05
    pasta da aula 04-05, arduino I

---
+++ Aula_04-05-22/20345_21101_21106_RGB_PWM_040522/20345_21101_21106_RGB_PWM_040522.ino
@@ -0,0 +1,120 @@
+/*IES: Cotuca/Unicamp
+Disciplina: Ds304 - Automação e Controle de Dispositivos
+Data: 04 Maio 2022
+Código realizado por:
+RA 20345 - Guilherme Pavezzi
+RA 21101 - Leandro Freitas
+RA 21106 - Lunara Cunha
+
+Nome: Atividade - Uso do led RGB como se fosse PWM
+Uso do Kit Arduíno Uno em ambiente  Arduíno IDE. 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+/*
+digitalWrite (LedRGB[0], LOW);
+digitalWrite (LedRGB[1], LOW);
+digitalWrite (LedRGB[2], HIGH);
+*/
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+      for (int i=0; i<256; i++){
+        analogWrite(3,i);
+        delay(50);
+        analogWrite(3,0);
+        }
+
+        for(int i=0; i>255; i++){
+          analogWrite(3,255-i);
+          delay(100);
+          analogWrite(3,0);
+          }
+
+        for (int i=0; i<256; i++){
+        analogWrite(5,i);
+        delay(150);
+        analogWrite(5,0);
+        }
+
+        for(int i=0; i>255; i++){
+          analogWrite(5,255-i);
+          delay(200);
+          analogWrite(5,0);
+          }          
+         for (int i=0; i<256; i++){
+        analogWrite(6,i);
+        delay(250);
+        analogWrite(6,0);
+        }
+
+        for(int i=0; i>255; i++){
+          analogWrite(6,255-i);
+          delay(300);
+          analogWrite(6,0);
+          }   
+
+/*
+
+  for (int i =0; i<256; i++) {
+    analogWrite(3,i);
+    delay(50);
+    analogWrite(3,0);
+     analogWrite(5,i);
+    delay(55);
+     analogWrite(5,0);
+     analogWrite(6,i);
+    delay(67);
+     analogWrite(6,0);
+    }
+    for (int i = 255; i>0; i--)
+    {
+     analogWrite(3,i);
+     delay(80);
+     analogWrite(3,0);
+     analogWrite(5,i);
+     delay(87);
+     analogWrite(5,i);
+    analogWrite(6,i);
+     delay(95);
+     analogWrite(6,0);
+      }
+
+      for (int i=0; i<256; i++){
+        analogWrite(3,i);
+        delay(50);
+        }
+
+        for(int i=0; i>255; i++){
+          analogWrite(2,255-i);
+          delay(100);
+          }
+          */
+/* Testando intensidades
+digitalWrite (LedRGB[0], HIGH);
+digitalWrite (LedRGB[1], LOW);
+digitalWrite (LedRGB[2], LOW);
+delay (500);
+digitalWrite (LedRGB[0], LOW);
+digitalWrite (LedRGB[1], HIGH);
+digitalWrite (LedRGB[2], LOW);
+delay (1000);
+digitalWrite (LedRGB[0], LOW);
+digitalWrite (LedRGB[1], LOW);
+digitalWrite (LedRGB[2], HIGH);
+delay (1500);
+digitalWrite (LedRGB[0], LOW);
+digitalWrite (LedRGB[1], LOW);
+digitalWrite (LedRGB[2], LOW);
+delay (2000);
+digitalWrite (LedRGB[0], HIGH);
+digitalWrite (LedRGB[1], HIGH);
+digitalWrite (LedRGB[2], HIGH);
+delay (2000);
+*/
+}
