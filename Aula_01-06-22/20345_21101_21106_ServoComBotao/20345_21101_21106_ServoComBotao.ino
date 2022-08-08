818f349f4e6f90b9796a3df38ebaf2d165f31ec7
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-08-08 20:01:03 -0300

    pasta 01-06
    pasta 01-06, arduino I

---
+++ Aula_01-06-22/20345_21101_21106_ServoComBotao/20345_21101_21106_ServoComBotao.ino
@@ -0,0 +1,48 @@
+/* Aula Ds304 - Dia 01-06-2022
+ *  Atividade sobre Motor Servo
+ *  RAs: 
+ *  20345 - Guilherme Pavezzi
+ *  21101 - Leandro Freitas*  
+ *  21106 - Lunara Cunha
+ *
+*/
+// Include the Servo library 
+ 
+#include <Servo.h>
+Servo servo1;
+  #define sPin 3
+  #define btn 7
+  #define frase "Iniciando"
+
+  
+
+  void setup(){
+    Serial.begin(9600);
+    Serial.println(frase);
+    servo1.attach(sPin);
+     pinMode(btn, INPUT);
+    }
+// Primeiro vai de 0 a 120; depois ao chegar em 120 ele volta para a posição 0;
+    void loop(){
+    
+      if(digitalRead(btn) == HIGH){
+      for (int i = 0; i<=120; i+=30){
+        servo1.write(i);
+        Serial.print("angulo :");
+        Serial.println(i);
+        delay(100);
+        if (i == 120){
+        for (int i = 120; i>=0; i-=30){
+          servo1.write(i);
+        Serial.println(" Estou retornando");
+        Serial.print("angulo :");
+        Serial.println(i);
+        delay(100);
+          }
+      }
+      }
+      
+
+      }
+
+    } 
