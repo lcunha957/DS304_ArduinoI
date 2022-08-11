49ab4ce1c3687c862563ca16b4f5523bdd189f8b
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-08-11 09:37:26 -0300

    Update 20345_21101_21106_RGB_PWM_040522.ino

--- Aula_04-05-22/20345_21101_21106_RGB_PWM_040522/20345_21101_21106_RGB_PWM_040522.ino
+++ Aula_04-05-22/20345_21101_21106_RGB_PWM_040522/20345_21101_21106_RGB_PWM_040522.ino
@@ -7,7 +7,8 @@ RA 21101 - Leandro Freitas
 RA 21106 - Lunara Cunha
 
 Nome: Atividade - Uso do led RGB como se fosse PWM
-Uso do Kit Arduíno Uno em ambiente  Arduíno IDE. 
+Uso do Kit Arduíno Uno em ambiente  Arduíno IDE.  
+Código no TinkerCad: https://www.tinkercad.com/things/6hOZAi2UVci?sharecode=A5n8EIEOnUcfsgPyWI_uoe38jxFT-IVcMq2BWJngxxc 
 */
 
 void setup() {
