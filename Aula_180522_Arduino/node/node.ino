729ae2752df3a02d0f3de958284aec1f7dcea115
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-08-11 09:09:01 -0300

    Update node.ino
    Código funcional

--- Aula_180522_Arduino/node/node.ino
+++ Aula_180522_Arduino/node/node.ino
@@ -1,10 +1,22 @@
-// the setup function runs once when you press reset or power the board
+// Aula realizada em 18-05-2022 pelos RAs:
+// 21101 - Leandro Freitas
+// 21106 - Lunara Cunha
+// Disciplina: DS303, curso 59 noturno
+
+
+int led1 = 2;
+int led2 = 4;
+int led3 = 7;
+
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(9600); // Begen listening on port 9600 for serial
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode (led3, OUTPUT);
 }
+
 // the loop function runs over and over again forever
 void loop() {
    if(Serial.available() > 0) // Read from serial port
@@ -16,11 +28,40 @@ void loop() {
   delay(1000);
 }
 void convertToState(char chr) {
-  if(chr=='o'){
-    digitalWrite(LED_BUILTIN, HIGH);
+  if(chr=='w'){
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
     delay(100);
   }
-  if(chr=='f'){
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-} }
+  if(chr=='b'){
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    delay(150);
+  }
+    if(chr=='x'){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    delay(200);
+  }
+    if(chr=='d'){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    delay(130);
+  }
+    if(chr=='r'){
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    delay(170);
+  }
+  if(chr=='t'){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    delay(180);
+}
+}
